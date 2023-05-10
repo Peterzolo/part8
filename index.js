373d5a828e16c9bdad8471d0611ffaec8e0f59b7
Peter <petersolomon704@gmail.com>
2023-05-10 11:09:36 +0100

    schema
    Added schema

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 
 const app = express();
+
+app.use("/graphql", graphqlHTTP({}));
 
 const PORT = process.env.PORT || 5000;
 
