2782a9657a95561205b5d3f6fd4a7dea0b3cb1fc
Peter <petersolomon704@gmail.com>
2023-05-20 15:19:32 +0100

    Update context.js
    Modified code

--- src/context/context.js
+++ src/context/context.js
@@ -1,41 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const getUser = async (token) => {
-  try {
-    if (token) {
-      const user = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-      return user;
+export const context = async ({ req }) => {
+  const token = req.headers.authorization;
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      req.authorId = decodedToken.authorId;
+    } catch (error) {
+      throw new Error("Invalid token");
     }
-    return null;
-  } catch (error) {
-    return null;
+  } else {
+    throw new Error("Authorization token not found");
   }
-};
-
-export const context = async ({ req, res }) => {
-  //   console.log(req.body.operationName);
-  if (req.body.operationName === "IntrospectionQuery") {
-    // console.log('blocking introspection query..');
-    return {};
-  }
-  // allowing the 'CreateUser' and 'Login' queries to pass without giving the token
-  if (
-    req.body.operationName === "CreateUser" ||
-    req.body.operationName === "Login"
-  ) {
-    return {};
-  }
-
-  // get the user token from the headers
-  const token = req.headers.authorization || "";
-
-  // try to retrieve a user with the token
-  const user = await getUser(token);
-
-  if (!user) {
-    throw new Error("User not fount");
-  }
-
-  // add the user to the context
-  return { user };
 };
